3b699709a95e4a7b3aac88608108ad5e1d5c8635
q-zh <41010137+q-zh@users.noreply.github.com>
2020-01-29 01:49:51 +0800

    Add files via upload

---
+++ main_alignment.m
@@ -0,0 +1,22 @@
+
+clear;
+addpath(genpath('depend'));
+data_folder = './data1/';
+process_data;
+
+Options.normalWeighting = 0;
+Options.plot = 1;
+
+our_output = in_template;
+tic
+[pointsTransformed, X] = our_nricp(in_template, input, Options, trans_para.index1(1:end), trans_para.index2(1:end));
+toc
+[our_output.vertices, landmark] = transform_back(pointsTransformed, trans_para);
+figure;ShowFace(input);view(180,90);
+
+nricp_output = in_template;
+tic
+[pointsTransformed, X] = nricp(in_template, input, Options, trans_para.index1(1:end), trans_para.index2(1:end));
+toc
+[nricp_output.vertices, landmark] = transform_back(pointsTransformed, trans_para);
+figure;ShowFace(nricp_output);view(180,90);
